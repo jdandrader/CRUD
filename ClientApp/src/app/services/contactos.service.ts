957e72074c3c4474ae4ef3dd6c5b587324c677bd
Nicolas-alt <a10100105555@gmail.com>
2020-10-22 16:33:16 -0500

    creo que ya

--- ClientApp/src/app/services/contactos.service.ts
+++ ClientApp/src/app/services/contactos.service.ts
@@ -7,32 +7,33 @@ import { Contacto } from '../models/icontactos';
   providedIn: 'root'
 })
 export class ContactosService {
- myAppUrl = 'https://localhost:44390/api/contactos/';
+ myAppUrl = 'https://localhost:44390/';
+ myAppiUrl = 'api/contactos/'
 
   httpOptions = {
     headers: new HttpHeaders ({
       'Contect-Type': 'application/json'
-    }) 
+    })
   }
   constructor(private http: HttpClient) {
-    console.log('Servicio Persona')  
+    console.log('Servicio Persona')
 
-  }  
+  }
 
   getListaContat(): Observable<Contacto[]> {
     return this.http.get<Contacto[]>(this.myAppUrl);
   }
 
-  deleteContacto(Id_usuario:number): Observable<Contacto[]>{
-    return this.http.delete<Contacto[]>(this.myAppUrl + Id_usuario);
+  deleteContacto(Id_usuario:number): Observable<Contacto>{
+    return this.http.delete<Contacto>(this.myAppUrl + this.myAppiUrl + Id_usuario);
   }
   guardarContcato(contacto: Contacto): Observable<Contacto> {
-    return this.http.post<Contacto>(this.myAppUrl , contacto, this.httpOptions);
+    return this.http.post<Contacto>(this.myAppUrl + this.myAppiUrl, contacto, this.httpOptions);
   }
   cargarContacto(Id_usuario:number): Observable<Contacto> {
-    return this.http.get<Contacto>(this.myAppUrl + Id_usuario);
+    return this.http.get<Contacto>(this.myAppUrl + this.myAppiUrl + Id_usuario);
   }
   actualizarContacto(Id_usuario:number,contacto:Contacto): Observable <Contacto>{
-    return this.http.put<Contacto>(this.myAppUrl  + Id_usuario, contacto, this.httpOptions);
+    return this.http.put<Contacto>(this.myAppUrl + this.myAppiUrl, contacto, this.httpOptions);
   }
 }
